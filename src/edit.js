03a919b660a69ab76ad349ddd66f3bb044b76260
Renan Rollo <renanrollo@MacBook-Pro-de-Renan.local>
2019-11-25 23:10:19 -0300

    Moving Tabs to another file

--- src/edit.js
+++ src/edit.js
@@ -1,6 +1,8 @@
+import Tabs from './components/Tabs';
+
 const { __ } = wp.i18n;
 
-const { useState, useEffect } = wp.element;
+const { useEffect } = wp.element;
 
 const {
   MediaPlaceholder,
@@ -17,11 +19,9 @@ const {
   PanelBody,
   TextareaControl,
   ExternalLink,
-  ButtonGroup,
   Toolbar,
   IconButton,
   SelectControl,
-  TabPanel,
 } = wp.components;
 
 function edit({ attributes, className, setAttributes }) {
@@ -32,7 +32,6 @@ function edit({ attributes, className, setAttributes }) {
     mediaAlt,
     toggleField,
     mediaSource,
-    sizes,
     type,
     loading,
     sourceTag,
@@ -41,15 +40,8 @@ function edit({ attributes, className, setAttributes }) {
     sourceMedia,
     smallThumb,
     mediumThumb,
-    originalSizeThumb,
-    smallSizeThumb,
-    mediumSizeThumb,
-    originalSourceThumb,
-    smallSourceThumb,
-    mediumSourceThumb,
     sourceThumb,
     sourceThumb2,
-    sourceSize,
     sourceScale,
   } = attributes;
 
@@ -126,30 +118,6 @@ function edit({ attributes, className, setAttributes }) {
     setAttributes({ mediaAlt: newAlt });
   };
 
-  const onChangeSizeOriginalThumb = newValue => {
-    setAttributes({ originalSizeThumb: newValue });
-  };
-
-  const onChangeSizeSmallThumb = newValue => {
-    setAttributes({ smallSizeThumb: newValue });
-  };
-
-  const onChangeSizeMediumThumb = newValue => {
-    setAttributes({ mediumSizeThumb: newValue });
-  };
-
-  const onChangeSourceOriginalThumb = newValue => {
-    setAttributes({ originalSourceThumb: newValue });
-  };
-
-  const onChangeSourceSmallThumb = newValue => {
-    setAttributes({ smallSourceThumb: newValue });
-  };
-
-  const onChangeSourceMediumThumb = newValue => {
-    setAttributes({ mediumSourceThumb: newValue });
-  };
-
   const onChangeSourceTag = newValue => {
     setAttributes({ sourceTag: newValue });
   };
@@ -174,33 +142,11 @@ function edit({ attributes, className, setAttributes }) {
     setAttributes({ sourceType });
   };
 
-  // Set the initial state of the Component
-  const [size, setSizes] = useState(sizes);
-  const [sizeSrc, setSizeSrc] = useState(sourceSize);
-
-  // Update the Component after new state is set
-  // Equivalent to componentDidMount in Classes
-  useEffect(() => {
-    setAttributes({ sizes: size });
-
-    // Starts after each new render. Here the variables determines when the function is called
-  }, [size]);
-
-  useEffect(() => {
-    setAttributes({ sourceSize: sizeSrc });
-  }, [sizeSrc]);
-
   useEffect(() => {
     if (toggleField === false) {
       setAttributes({
-        smallSizeThumb: undefined,
-        mediumSizeThumb: undefined,
-        originalSizeThumb: undefined,
         smallThumb: undefined,
         mediumThumb: undefined,
-        smallSourceThumb: undefined,
-        mediumSourceThumb: undefined,
-        originalSourceThumb: undefined,
         sourceThumb: undefined,
         sourceThumb2: undefined,
       });
@@ -691,184 +637,7 @@ function edit({ attributes, className, setAttributes }) {
                 </div>
               )}
             </PanelBody>
-
-            <PanelBody title={__('Image Settings')} initialOpen={false}>
-              <TabPanel
-                className="uagb-inspect-tabs uagb-inspect-tabs-col-2"
-                activeClass="active-tab"
-                tabs={[
-                  {
-                    name: 'Source',
-                    title: __('Source'),
-                    className: 'picture-thumb-source',
-                  },
-                  {
-                    name: 'Source 2',
-                    title: __('Source 2'),
-                    className: 'picture-thumb-source',
-                  },
-                ]}
-              >
-                {tab => {
-                  let tabout;
-
-                  if (tab.name === 'Source') {
-                    tabout = (
-                      <>
-                        <TextControl
-                          label={__('Original image')}
-                          help={
-                            <>
-                              <ExternalLink href="https://www.html5rocks.com/en/tutorials/responsive/picture-element/#toc-pixel-density-descriptors">
-                                {__(
-                                  'Combine with pixel density or width descriptors'
-                                )}
-                              </ExternalLink>
-                              {__('Leave blank if using pixel method.')}
-                            </>
-                          }
-                          value={originalSizeThumb}
-                          onChange={onChangeSizeOriginalThumb}
-                        />
-
-                        <div className="block-library-image__dimensions">
-                          <p className="block-library-image__dimensions__row">
-                            {__('Thumbnails Scale')}
-                          </p>
-
-                          <div className="block-library-image__dimensions__row">
-                            <TextControl
-                              className="block-library-image__dimensions__width"
-                              label={__('Small')}
-                              help={__('e.g., 300w or 1x')}
-                              value={smallSizeThumb}
-                              onChange={onChangeSizeSmallThumb}
-                            />
-                            <TextControl
-                              className="block-library-image__dimensions__height"
-                              label={__('Medium')}
-                              help={__('e.g., 768w or 2x')}
-                              value={mediumSizeThumb}
-                              onChange={onChangeSizeMediumThumb}
-                            />
-                          </div>
-                          <div className="block-library-image__dimensions__row">
-                            <div>
-                              <p className="block-library-image__dimensions__row">
-                                {__('Sizes attribute')}
-                              </p>
-                              <p className="components-base-control__help">
-                                {__('Leave unselected if using pixel method.')}
-                              </p>
-
-                              <ButtonGroup aria-label={__('Image Size')}>
-                                {['25vw', '50vw', '75vw', '100vw'].map(
-                                  scale => {
-                                    return (
-                                      <Button
-                                        key={scale}
-                                        isSmall
-                                        isPrimary={size === scale}
-                                        aria-pressed={size === scale}
-                                        onClick={() =>
-                                          setSizes(
-                                            size === scale ? null : scale
-                                          )
-                                        }
-                                      >
-                                        {scale}
-                                      </Button>
-                                    );
-                                  }
-                                )}
-                              </ButtonGroup>
-                            </div>
-                          </div>
-                        </div>
-                      </>
-                    );
-                  } else {
-                    tabout = (
-                      <>
-                        <TextControl
-                          label={__('Original image')}
-                          help={
-                            <>
-                              <ExternalLink href="https://www.html5rocks.com/en/tutorials/responsive/picture-element/#toc-pixel-density-descriptors">
-                                {__(
-                                  'Combine with pixel density or width descriptors'
-                                )}
-                              </ExternalLink>
-                              {__('Leave blank if using pixel method.')}
-                            </>
-                          }
-                          value={originalSourceThumb}
-                          onChange={onChangeSourceOriginalThumb}
-                        />
-
-                        <div className="block-library-image__dimensions">
-                          <p className="block-library-image__dimensions__row">
-                            {__('Thumbnails Scale')}
-                          </p>
-
-                          <div className="block-library-image__dimensions__row">
-                            <TextControl
-                              className="block-library-image__dimensions__width"
-                              label={__('Small')}
-                              help={__('e.g., 300w or 1x')}
-                              value={smallSourceThumb}
-                              onChange={onChangeSourceSmallThumb}
-                            />
-                            <TextControl
-                              className="block-library-image__dimensions__height"
-                              label={__('Medium')}
-                              help={__('e.g., 768w or 2x')}
-                              value={mediumSourceThumb}
-                              onChange={onChangeSourceMediumThumb}
-                            />
-                          </div>
-                          <div className="block-library-image__dimensions__row">
-                            <div>
-                              <p className="block-library-image__dimensions__row">
-                                {__('Sizes attribute')}
-                              </p>
-                              <p className="components-base-control__help">
-                                {__('Leave unselected if using pixel method.')}
-                              </p>
-
-                              <ButtonGroup aria-label={__('Image Size')}>
-                                {['25vw', '50vw', '75vw', '100vw'].map(
-                                  scalesrc => {
-                                    return (
-                                      <Button
-                                        key={scalesrc}
-                                        isSmall
-                                        isPrimary={sizeSrc === scalesrc}
-                                        aria-pressed={sizeSrc === scalesrc}
-                                        onClick={() =>
-                                          setSizeSrc(
-                                            sizeSrc === scalesrc
-                                              ? null
-                                              : scalesrc
-                                          )
-                                        }
-                                      >
-                                        {scalesrc}
-                                      </Button>
-                                    );
-                                  }
-                                )}
-                              </ButtonGroup>
-                            </div>
-                          </div>
-                        </div>
-                      </>
-                    );
-                  }
-                  return <div>{tabout}</div>;
-                }}
-              </TabPanel>
-            </PanelBody>
+            <Tabs toggle={!!toggleField} />
           </>
         )}
       </InspectorControls>
